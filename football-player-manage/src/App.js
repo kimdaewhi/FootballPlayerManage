a41dfdd1ab3698a3c5acffc0af1286a97391d9b9
김대휘 <kimdaewhi@naver.com>
2023-10-25 22:10:11 +0900

    temp

--- football-player-manage/src/App.js
+++ football-player-manage/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const players = useSelector((state) => state.players);
   return (
     <div className="App">
-      <button 
+      <button style={{marginBottom: "15px"}}
         onClick={(e) => {
             console.log(players);
           }}>Test</button>
